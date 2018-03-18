5c47691016bb803542b5bf633ecd5daa266f5e06
Ahmed Abdelwahab <thediamondknight1@gmail.com>
2018-03-18 12:55:35 +0200

    merged sw-support with master. Commented registeration snippet to be uncommented if needed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,21 @@
+function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(registration => {
+          console.log('SW registered: ', registration);
+        })
+        .catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+        });
+    });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', ev => {
   console.log('Hello World!');
 });
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
-}
+// Uncomment for service worker support
+// registerServiceWorker();
