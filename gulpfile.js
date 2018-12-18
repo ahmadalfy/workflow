9735db3f0addfced844f9770adc1c20f68af1eb8
Ahmad Alfy <ahmadalfy@gmail.com>
2018-12-18 16:05:07 +0200

    Massive refactor for gulpfile.js
    - Migrate to gulp 4
    - Refactor gulpfile.js to use the new API
    - Remove run-sequence for the favor of gulp.parallel and gulp.series
    - Remove revision tasks and packages
    - Gulpfile linted with ESLint
    - Update Modernizr to 3.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Configurable paths
-var config = {
+let config = {
   src: 'src',
   dist: 'dist'
 };
@@ -7,39 +7,37 @@ const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
 const del = require('del');
-const runSequence = require('run-sequence');
 const svgSprite = require('gulp-svg-sprite');
 const babel = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const revAll = require('gulp-rev-all');
 const gulpStylelint = require('gulp-stylelint');
 const workbox = require('workbox-build');
 
 const $ = gulpLoadPlugins();
-const reload = browserSync.reload;
+const { reload } = browserSync;
 
-var dev = true;
-gulp.task('styles', () => {
-  return gulp
+let dev = true;
+
+gulp.task('styles', () =>
+  gulp
     .src('./src/styles/*.css')
     .pipe(sourcemaps.init())
     .pipe($.plumber())
     .pipe($.postcss())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({ stream: true }));
-});
+    .pipe(reload({ stream: true }))
+);
 
 gulp.task('scripts', () => {
   const bundler = browserify('src/scripts/main.js', { debug: dev }).transform(babel);
   return bundler
     .bundle()
-    .on('error', function(err) {
-      console.error(err);
+    .on('error', err => {
+      console.error(err); /* eslint-disable-line */
       this.emit('end');
     })
     .pipe(source('main.js'))
@@ -58,51 +56,19 @@ function lint(files) {
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
 }
 
-gulp.task('lint:js', () => {
-  return lint('src/scripts/**/*.js').pipe(gulp.dest('src/scripts'));
-});
+gulp.task('lint:js', () => lint('src/scripts/**/*.js').pipe(gulp.dest('src/scripts')));
 
-gulp.task('lint:test', () => {
-  return lint('test/spec/**/*.js').pipe(gulp.dest('test/spec'));
-});
+gulp.task('lint:test', () => lint('test/spec/**/*.js').pipe(gulp.dest('test/spec')));
 
-gulp.task('views', () => {
-  return gulp
+gulp.task('views', () =>
+  gulp
     .src('src/*.pug')
     .pipe($.plumber())
-    .pipe(
-      $.data(function(file) {
-        return { require: require };
-      })
-    )
+    .pipe($.data(() => ({ require })))
     .pipe($.pug({ pretty: true }))
     .pipe(gulp.dest('.tmp'))
-    .pipe(reload({ stream: true }));
-});
-
-gulp.task('html', ['lint:css', 'lint:js', 'views', 'styles', 'scripts'], () => {
-  return gulp
-    .src('.tmp/**/*[.html, .js, .css]')
-    .pipe($.useref({ searchPath: ['.tmp', 'src', '.'] }))
-    .pipe($.if(/\.js$/, $.uglify({ compress: { drop_console: true } })))
-    .pipe($.if(/\.css$/, $.cssnano({ safe: true, autoprefixer: false })))
-    .pipe(
-      $.if(
-        /\.html$/,
-        $.htmlmin({
-          collapseWhitespace: false,
-          minifyCSS: true,
-          minifyJS: { compress: { drop_console: true } },
-          processConditionalComments: true,
-          removeComments: true,
-          removeEmptyAttributes: false,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true
-        })
-      )
-    )
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(reload({ stream: true }))
+);
 
 gulp.task('svgSprite', () => {
   config = {
@@ -120,11 +86,9 @@ gulp.task('svgSprite', () => {
     .pipe(gulp.dest('src/images/'));
 });
 
-gulp.task('fonts', () => {
-  return gulp
-    .src('src/fonts/**/*')
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
-});
+gulp.task('fonts', () =>
+  gulp.src('src/fonts/**/*').pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')))
+);
 
 function modernizerTask(dest) {
   return () => {
@@ -135,39 +99,65 @@ function modernizerTask(dest) {
       .pipe(gulp.dest(`${dest}/scripts/vendor/`));
   };
 }
+gulp.task(
+  'modernizr:serve',
+  gulp.series(done => {
+    modernizerTask('.tmp');
+    done();
+  })
+);
+gulp.task(
+  'modernizr:build',
+  gulp.series(done => {
+    modernizerTask('dist');
+    done();
+  })
+);
 
-gulp.task('modernizr:serve', modernizerTask('.tmp'));
-
-gulp.task('modernizr:build', modernizerTask('dist'));
-
-gulp.task('rev', () => {
+gulp.task('extras', () =>
   gulp
-    .src('dist/**/*.{js,css}')
-    .pipe(revAll.revision({ debug: true }))
-    .pipe(gulp.dest('dist/'))
-    .pipe(revAll.versionFile())
-    .pipe(gulp.dest('dist/'))
-    .pipe(revAll.manifestFile())
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('extras', () => {
-  return gulp
     .src(['src/*', '!src/*.html', '!src/*.pug', '!src/includes'], {
       dot: true
     })
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+);
 
-gulp.task('lint:css', function lintCssTask() {
-  return gulp.src('src/styles/**/*.css').pipe(
+gulp.task('lint:css', () =>
+  gulp.src('src/styles/**/*.css').pipe(
     gulpStylelint({
       reporters: [{ formatter: 'string', console: true }]
     })
-  );
-});
+  )
+);
 
-//Pre-caching
+gulp.task(
+  'html',
+  gulp.series(['lint:css', 'lint:js', 'views', 'styles', 'scripts'], () =>
+    gulp
+      .src('.tmp/**/*[.html, .js, .css]')
+      .pipe($.useref({ searchPath: ['.tmp', 'src', '.'] }))
+      .pipe($.if(/\.js$/, $.uglify({ compress: { drop_console: true } })))
+      .pipe($.if(/\.css$/, $.cssnano({ safe: true, autoprefixer: false })))
+      .pipe(
+        $.if(
+          /\.html$/,
+          $.htmlmin({
+            collapseWhitespace: false,
+            minifyCSS: true,
+            minifyJS: { compress: { drop_console: true } },
+            processConditionalComments: true,
+            removeComments: true,
+            removeEmptyAttributes: false,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+          })
+        )
+      )
+      .pipe(gulp.dest('dist'))
+  )
+);
+
+// Pre-caching
 function generateSW(dist) {
   return workbox.generateSW({
     globDirectory: dist,
@@ -184,41 +174,41 @@ function generateSW(dist) {
   });
 }
 
-gulp.task('sw:build', () => {
-  return generateSW('dist')
+gulp.task('sw:build', () =>
+  generateSW('dist')
     .then(({ warnings }) => {
       // In case there are any warnings from workbox-build, log them.
       for (const warning of warnings) {
-        console.warn(warning);
+        console.warn(warning); /* eslint-disable-line */
       }
-      console.info('Service worker generation completed.');
+      console.info('Service worker generation completed.'); /* eslint-disable-line */
     })
     .catch(error => {
-      console.warn('Service worker generation failed:', error);
-    });
-});
+      console.warn('Service worker generation failed:', error); /* eslint-disable-line */
+    })
+);
 
-gulp.task('sw:serve', () => {
-  return generateSW('.tmp')
+gulp.task('sw:serve', () =>
+  generateSW('.tmp')
     .then(({ warnings }) => {
       // In case there are any warnings from workbox-build, log them.
       for (const warning of warnings) {
-        console.warn(warning);
+        console.warn(warning); /* eslint-disable-line */
       }
-      console.info('Service worker generation completed.');
+      console.info('Service worker generation completed.'); /* eslint-disable-line */
     })
     .catch(error => {
-      console.warn('Service worker generation failed:', error);
-    });
-});
+      console.warn('Service worker generation failed:', error); /* eslint-disable-line */
+    })
+);
 
 // I dont need to clean dist as the clean task is called on serve only
 gulp.task('clean:build', del.bind(null, ['dist']));
 gulp.task('clean:serve', del.bind(null, ['.tmp']));
 
 // @Task is used to optimize images in src directory
-gulp.task('optimize:images', () => {
-  return gulp
+gulp.task('optimize:images', () =>
+  gulp
     .src('src/images/**/*')
     .pipe(
       $.cache(
@@ -234,54 +224,77 @@ gulp.task('optimize:images', () => {
         )
       )
     )
-    .pipe(gulp.dest('dist/images'));
-});
+    .pipe(gulp.dest('dist/images'))
+);
 
-gulp.task('images', () => {
-  return gulp
-    .src('src/images/**/*')
-    .pipe($.if(dev, gulp.dest('.tmp/images'), gulp.dest('dist/images')));
-});
+gulp.task('images', () =>
+  gulp.src('src/images/**/*').pipe($.if(dev, gulp.dest('.tmp/images'), gulp.dest('dist/images')))
+);
+
+// @Task Default task to run when you run `gulp` with no arguments
+gulp.task(
+  'default',
+  gulp.series(
+    done =>
+      new Promise(resolve => {
+        gulp.series('serve', resolve);
+        done();
+      })
+  )
+);
 
 // @Task unused one
-gulp.task('serve:dist', ['default'], () => {
-  browserSync.init({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist']
-    }
-  });
-});
-
-// @Task unused one
-gulp.task('serve:test', ['scripts'], () => {
-  browserSync.init({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-        '/bower_components': 'bower_components'
+gulp.task(
+  'serve:dist',
+  gulp.series(['default'], () => {
+    browserSync.init({
+      notify: false,
+      port: 9000,
+      server: {
+        baseDir: ['dist']
       }
-    }
-  });
+    });
+  })
+);
 
-  gulp.watch('src/scripts/**/*.js', ['scripts']);
-  gulp.watch(['test/spec/**/*.js', 'test/index.html']).on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
-});
+// @Task unused one
+gulp.task(
+  'serve:test',
+  gulp.series(['scripts'], () => {
+    browserSync.init({
+      notify: false,
+      port: 9000,
+      ui: false,
+      server: {
+        baseDir: 'test',
+        routes: {
+          '/scripts': '.tmp/scripts'
+        }
+      }
+    });
+
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+    gulp.watch(['test/spec/**/*.js', 'test/index.html']).on('change', reload);
+    gulp.watch('test/spec/**/*.js', ['lint:test']);
+  })
+);
 
 // @Task `serve` is used to serve the content in dev mode
-gulp.task('serve', () => {
-  runSequence(
-    ['clean:serve'],
-    ['lint:js', 'lint:css'],
-    ['views', 'styles', 'scripts', 'fonts', 'svgSprite'],
-    ['modernizr:serve'],
-    ['sw:serve'],
+gulp.task(
+  'serve',
+  gulp.series(
+    [
+      'clean:serve',
+      'lint:js',
+      'lint:css',
+      'views',
+      'styles',
+      'scripts',
+      'fonts',
+      'svgSprite',
+      'modernizr:serve',
+      'sw:serve'
+    ],
     () => {
       browserSync.init({
         notify: false,
@@ -295,33 +308,33 @@ gulp.task('serve', () => {
       });
 
       gulp.watch(['.tmp/*.html', 'src/images/**/*', '.tmp/fonts/**/*']).on('change', reload);
-      gulp.watch('src/**/*.pug', ['views']);
-      gulp.watch('src/styles/**/*.css', ['styles', 'lint:css']);
-      gulp.watch('src/scripts/**/*.js', ['scripts', 'lint:js']);
-      gulp.watch('src/images/**/*', ['optimize:images']);
-      gulp.watch('src/fonts/**/*', ['fonts']);
-      gulp.watch('bower.json', ['fonts']);
+      gulp.watch('src/**/*.pug', gulp.series('views'));
+      gulp.watch('src/styles/**/*.css', gulp.series(['styles', 'lint:css']));
+      gulp.watch('src/scripts/**/*.js', gulp.series(['scripts', 'lint:js']));
+      gulp.watch('src/images/**/*', gulp.series('optimize:images'));
+      gulp.watch('src/fonts/**/*', gulp.series('fonts'));
     }
-  );
-});
+  )
+);
 
 // @Task `build` is used to build a production ready dist
-gulp.task('build', () => {
-  dev = false;
-  runSequence(
-    ['clean:build'],
-    ['html', 'svgSprite', 'fonts', 'extras'],
-    ['rev', 'modernizr:build', 'images'],
-    ['sw:build'],
-    () => {
-      return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }));
-    }
-  );
-});
-
-// @Task Default task to run when you run `gulp` with no arguments
-gulp.task('default', () => {
-  return new Promise(resolve => {
-    runSequence('serve', resolve);
-  });
-});
+gulp.task(
+  'build',
+  gulp.series(
+    [
+      done => {
+        dev = false;
+        done();
+      },
+      'clean:build',
+      'html',
+      'svgSprite',
+      'fonts',
+      'extras',
+      'modernizr:build',
+      'images',
+      'sw:build'
+    ],
+    () => gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }))
+  )
+);
