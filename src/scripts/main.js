4094e9512f36d21f57b9ca1b3630cb6bb44bf43c
Ahmad Alfy <ahmadalfy@gmail.com>
2018-12-18 10:25:58 +0200

    Update ESLint and dependencies and add prettier

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,17 @@
-function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then(registration => {
-          console.log('SW registered: ', registration);
-        })
-        .catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
-        });
-    });
-  }
-}
-
-document.addEventListener('DOMContentLoaded', ev => {
-  console.log('Hello World!');
-});
+// function registerServiceWorker() {
+//   if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker
+//         .register('/sw.js')
+//         .then(registration => {
+//           console.log('SW registered: ', registration);
+//         })
+//         .catch(registrationError => {
+//           console.log('SW registration failed: ', registrationError);
+//         });
+//     });
+//   }
+// }
 
 // Uncomment for service worker support
 // registerServiceWorker();
