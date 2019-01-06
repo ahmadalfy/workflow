e78a7dfb8fee49cf6f885b5dd3a47de4d96f78af
Nadine Adel <nadine.adel@robustastudio.com>
2019-01-06 14:38:05 +0200

    Replaces spaces with tabs

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-logical': {},
-    'postcss-dir-pseudo-class': {},
-    'postcss-preset-env': { stage: 0 },
-    'postcss-nested': {},
-    'postcss-retina-bg-img': {
-      retinaSuffix: '@2x',
-    },
-  },
+	plugins: {
+		'postcss-import': {},
+		'postcss-logical': {},
+		'postcss-dir-pseudo-class': {},
+		'postcss-preset-env': { stage: 0 },
+		'postcss-nested': {},
+		'postcss-retina-bg-img': {
+			retinaSuffix: '@2x',
+		},
+	},
 };
