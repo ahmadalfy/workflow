f12fa2244b913ad828345cd2aecf85c806b2bc8e
Ahmad Alfy <ahmadalfy@gmail.com>
2019-04-11 21:58:53 +0200

    feat: add postcss-easy-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	plugins: {
 		'postcss-import': {},
+		'postcss-easy-import': {},
 		'postcss-logical': {},
 		'postcss-dir-pseudo-class': {},
 		'postcss-preset-env': { stage: 0 },
@@ -8,6 +9,5 @@ module.exports = {
 		'postcss-retina-bg-img': {
 			retinaSuffix: '@2x',
 		},
-		'postcss-easy-import': {},
 	},
 };
